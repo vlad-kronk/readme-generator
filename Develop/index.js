3b577d34d64845fac51ffc1ba218d9c2bc4692d3
Jacob Meyers <jacobmeyers6@outlook.com>
2022-11-19 23:17:26 -0800

    added rendering functions for each section, got inquirer prompts to work

--- Develop/index.js
+++ Develop/index.js
@@ -1,20 +1,61 @@
-// TODO: Include packages needed for this application
+// TODO: Include packages needed for this application,
+
+import generateMarkdown from './utils/generateMarkdown';
+
+var inquirer = require('inquirer');
+const fs = require('fs');
+
 
 // TODO: Create an array of questions for user input
 const questions = [
-    "Project title?",
-    "Project description?",
-    "Installation instructions?",
-    "Usage information?",
-    "Contribution instructions?",
-    "Test instructions?"
+
+    // "Project title?",
+    // "Project description?",
+    // "Installation instructions?",
+    // "Usage information?",
+    // "Contribution instructions?",
+    // "Test instructions?"
+
+    {name: 'input', message: 'Project title?'},
+
+    {
+        type: 'rawlist', name: 'license', message: 'License?',
+        choices: [
+            'MIT',
+            'Apache 2.0',
+            'GNU General Public v3.0',
+            'Eclipse Public License 2.0',
+            'Creative Commons Zero v1.0',
+            'The Unlicense',
+            'Mozilla Public License 2.0'
+        ]
+    },
+
+    {name: 'desc', message: 'Project description?'},
+    {name: 'install', message: 'Installation instructions?'},
+    {name: 'usage', message: 'Usage information?'},
+    {name: 'contr', message: 'Contribution instructions?'},
+    {name: 'test', message: 'Test instructions?'},
+    {name: 'githubID', message: 'Github username?'},
+    {name: 'email', message: 'Email?'},
+
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+    let prompt = inquirer.createPromptModule();
+
+    prompt(questions)
+        .then(answers => {
+            console.log(answers);
+
+        })
+
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,3 +1,8 @@
+// render title
+function renderTitle(title) {
+  return `## ${title}`;
+}
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
@@ -8,23 +13,62 @@ function renderLicenseBadge(license) {
     return `https://img.shields.io/badge/License-${license}-blue&style=for-the-badge`;
   }
   
-  return "";
+  return '';
 
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  
+  if (license) {
+    return '[License](#license)';
+  }
+
+  return '';
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+
+  let section = `## License\n\n![](${renderLicenseBadge(license)})\n`
+
+  return section;
+
+}
+
+// render table of contents
+function renderTableOfContents(arr) {
+  let section = `## Table of Contents\n\n`;
+  arr.forEach(item => {
+    section = `${section}-   [${item}](#${item.toLowerCase})\n`;
+  })
+}
+
+// render basic sections
+// -  description
+// -  installation instructions
+// -  usage information
+// -  contribution guidelines
+// -  test instructions
+function renderBasicSection(title, content) {
+  let section = `## ${title}\n\n${content}\n`
+  return section;
+}
+
+// render questions section
+function renderQuestions(githubID, email) {
+  let section = `## Questions\n\n`;
+  section = `${section}<a href="https://github.com/${githubID}>Github Profile</a>\n\n`;
+  section = `${section} Email <a href="mailto: ${email}">${email}</a> with any further questions.\n`;
+  return section;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+  
 }
 
 module.exports = generateMarkdown;
